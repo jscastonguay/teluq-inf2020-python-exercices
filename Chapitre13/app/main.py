779c802a85af056bb61fba645836bdd67edce5e1
Jean-Sébastien Castonguay <jscastonguay@gmail.com>
2024-11-13 20:39:39 -0500

    Affichage d'une liste de nom et ajout dans la liste à partir d'une méthode POST. La méthode GET est aussi codée mais est commentée.

--- Chapitre13/app/main.py
+++ Chapitre13/app/main.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import datetime
 
 app = Flask(__name__)
 
+liste_etudiants = ["Alex", "Julie", "Kevin"]
+
 @app.route("/")
 def root():
-    return render_template("principale.html", liste_etudiants = ["Alex", "Julie", "Kevin"])
+    return render_template("principale.html", liste_etudiants=liste_etudiants)
+
+@app.route("/ajoute", methods = ["POST"])
+def ajoute():
+    nom = request.form.get("nom")
+    liste_etudiants.append(nom)
+    return redirect("/")
+
+# @app.route("/ajoute", methods = ["GET"])
+# def ajoute():
+#     nom = request.args.get("nom")
+#     liste_etudiants.append(nom)
+#     return redirect("/")
 
 if __name__ == "__main__":
     app.run()
+    
