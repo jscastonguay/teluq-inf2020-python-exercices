114d1e69a4ec52c3a0619679e303e95bea32d183
Jean-Sébastien Castonguay <jscastonguay@gmail.com>
2024-12-18 20:30:28 -0500

    Ajoute docstring. Crée le répertoire si il n'existe pas. Gère les exceptions.

--- TravailNo3/app/liste_todo.py
+++ TravailNo3/app/liste_todo.py
@@ -18,6 +18,8 @@ class ErreurInterne(Exception):
 
 
 class ConditionsFiltre:
+    """Classe permettant d'appliquer un filtre sur la liste de TODO.
+    """
     def __init__(self) -> None:
         self.etats: list[Etat] = [Etat.OUVERT.name, Etat.EN_COURS.name, Etat.FERMEE.name]
         self.tags: list[str] = []
@@ -26,10 +28,18 @@ class ConditionsFiltre:
         self.etats: list[Etat] = [Etat.OUVERT.name, Etat.EN_COURS.name, Etat.FERMEE.name]
         self.tags: list[str] = []
         
-    def applique(self, element):
-        '''
-        Si les filtres sont vides, les éléments ne sont pas filtrés.
-        '''                
+    def applique(self, element: dict) -> bool:
+        """Vérifie si un TODO donné diot être filtré ou non.
+        
+        Noter que si les filtres sont vides, les éléments ne seront pas
+        filtrés.
+
+        Args:
+            element (dict): Un TODO.
+
+        Returns:
+            bool: True si le TODO ne doit pas être retiré de la liste filtrée.
+        """
         etatsTrouves = False
         if self.etats == [] or element["etat"] in self.etats:
             etatsTrouves = True
@@ -44,6 +54,9 @@ class ConditionsFiltre:
 
         
 class ListeTodo:
+    """Classe encapsulant la liste de TODO et définissant du même coup la
+    structure d'un TODO.
+    """
     
     # Singleton
     _repertoire: str = ""
@@ -59,6 +72,15 @@ class ListeTodo:
                 asyncio.run(self._recupere_liste(liste_fichiers))
 
     def nouveau(self, titre: str = "", description:str = "", tags: list[str] = []) -> None:
+        """Crée un nouveau TODO et le sauvegarde dans un fichier propre à ce
+        TODO dont le nom est un UUID.
+
+        Args:
+            titre (str, optional): Le titre du TODO. Defaults to "".
+            description (str, optional): La description du TODO. Defaults to "".
+            tags (list[str], optional): Une liste de tags associés au TODO.
+            Defaults to [].
+        """
         todo = dict()
         todo["uuid"] = str(uuid.uuid1())
         todo["date_creation"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -71,16 +93,14 @@ class ListeTodo:
         self._sauvegarde(todo)
         ListeTodo._liste.append(todo)
         
-    def _valide_uuid(self, texte: str):
+    def _valide_uuid(self, texte: str) -> bool:
         uuid_pattern = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"
         return re.match(uuid_pattern, texte)
 
-    def _get_liste_fichiers(self) -> list[str]:
-        
-        # TODO Gèrer le fait que le répertoire n'existe peut-être pas
-        #      Cérer le répertoire
-        
+    def _get_liste_fichiers(self) -> list[str]:        
         fichers_filtres = []
+        if not os.path.exists(self._repertoire): 
+            os.makedirs(self._repertoire)
         fichers = os.listdir(self._repertoire)
         for fichier in fichers:
             if not os.path.isdir(os.path.join(self._repertoire, fichier)):
@@ -96,9 +116,9 @@ class ListeTodo:
             todo = await f.read()
             ListeTodo._liste.append(json.loads(todo))
     
-    async def _recupere_liste(self, fichers_filtres: list[str]) -> None:
-        assert len(fichers_filtres) != 0
-        await asyncio.gather(*(self._recupere_todo(f) for f in fichers_filtres))
+    async def _recupere_liste(self, fichiers: list[str]) -> None:
+        assert len(fichiers) != 0
+        await asyncio.gather(*(self._recupere_todo(f) for f in fichiers))
     
     def _sauvegarde(self, todo: dict) -> None:
         if not os.path.exists(self._repertoire):
@@ -107,6 +127,24 @@ class ListeTodo:
             f.write(json.dumps(todo))
     
     def get(self, uuid: str = "", filtre: ConditionsFiltre = None) -> list[dict]:
+        """Récupère une liste de TODO selon deux mécanismes: soit par un numéro
+        de UUID, auquel cas ue seul TODO est présent dans la liste, soit par un
+        filtre défini par 'filtre'. Si aucun des mécanisme n'est sélectionné,
+        la méthode retourne toute la liste.
+
+        Args:
+            uuid (str, optional): UUID du TODO à récupérer. Defaults to "".
+            
+            filtre (ConditionsFiltre, optional): Le filtre à utiliser pour la
+            recherche. Defaults to None.
+
+        Raises:
+            ErreurInterne: Lorsqu'une erreur se produit reliée à la base de
+            données (répertoire et fichiers).
+
+        Returns:
+            list[dict]: Les TODO sélectionnés.
+        """
         if uuid:
             todo = list(filter(lambda element: element["uuid"] == uuid, ListeTodo._liste))
             if todo == None:
@@ -121,18 +159,29 @@ class ListeTodo:
             return ListeTodo._liste
         
     def enleve(self, uuid: str) -> None:
+        """Enlève un TODO de la liste.
+
+        Args:
+            uuid (str): Le UUID du TODO à enlever.
+        """
         todo = self.get(uuid)
         self._liste.remove(todo[0])
-        
-        # TODO géré les exceptions: si un fichier n'existe pas
         os.remove(f"{self._repertoire}/{uuid}.json")
         
     def modifie(self, todo: dict):
-        
-        #TODO géré les exceptions
+        """Modifie un TODO donnée. Son fichier correspondant sera aussi modifié.
+
+        Args:
+            todo (dict): La nouvelle valeur du TODO.
+        """
         index = next((i for i, a_modifier in enumerate(ListeTodo._liste) if a_modifier["uuid"] == todo["uuid"]), None)
         ListeTodo._liste[index] = todo
         self._sauvegarde(todo)
         
-    def get_nb(self):
+    def get_nb(self) -> int:
+        """Retourne le nombre de TODO total dans la liste.
+
+        Returns:
+            int: Le nombre de TODO.
+        """
         return len(ListeTodo._liste)
--- TravailNo3/app/main.py
+++ TravailNo3/app/main.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, abort, Response
 from liste_todo import *
-import sys
 
 
 app = Flask(__name__)
 liste = ListeTodo("todo")
 conditions_filtre = ConditionsFiltre()
 
+
 @app.route("/")
-def index():
-    listeComplete = liste.get(filtre=conditions_filtre)        
-    return render_template("index.html", liste = listeComplete, filtre = conditions_filtre)
+def index() -> str:
+    """Retourne la page d'accueil de l'application web.
+
+    Returns:
+        str: La page d'accueil en HTML.
+    """
+
+    try:
+        liste_filtree = liste.get(filtre=conditions_filtre) 
+    except:
+        abort(500)
+    return render_template("index.html", liste = liste_filtree, filtre = conditions_filtre)
 
 
 @app.route("/ajoute",  methods = ['GET', 'POST'])
-def ajoute():
+def ajoute() -> str | Response:
+    """Retourne la page permettant d'ajouter un TODO si la méthode est GET.
+       Si la méthode est POST, applique le nouveau TODO entré par l'usager
+       et redirige à la page d'accueil.
+
+    Returns:
+        str | Response: La page qui permet l'ajout ou une redirection à la
+        page d'accueil.
+    """
     if request.method == "GET":
         return render_template("ajoute.html")
     else:
@@ -22,29 +39,52 @@ def ajoute():
         description = request.form['description']
         tags = request.form['tags'].split(',')
         tags = [tag.strip() for tag in tags]
-        liste.nouveau(titre, description, tags)
+        try:
+            liste.nouveau(titre, description, tags)
+        except:
+            abort(500)
         return redirect("/")
    
    
 @app.route('/submit', methods=['POST'])
-def submit():
+def submit() -> Response:
+    """Applique une sélection à la liste de TODO.
+
+    Returns:
+        Response: Si l'action est 'efface', enlève le TODO de la liste et
+        redirige vers la page d'accueil. Si l'action est 'modifie' redirige
+        vers la page qui permet de modifier un TODO.
+    """
     if 'uuid_selectionnee' in request.form:
         uuid = request.form['uuid_selectionnee']
         action = request.form['action']
+        assert(action == "efface" or action == "modifie")
         if action == "efface":
-            liste.enleve(uuid)
+            try:
+                liste.enleve(uuid)
+            except:
+                abort(500)
             return redirect("/")
         if action == "modifie":
             return redirect(url_for("modifie", uuid = uuid))
-    
-    # TODO gère une erreur
     return redirect("/")
 
 
 @app.route('/modifie', methods=['GET', 'POST'])
-def modifie():        
+def modifie() -> str | Response:    
+    """Permet la modification d'un TODO.
+
+    Returns:
+        str | Response: La page qui permet la modification d'un TODO si la
+        méthode est GET ou applique les modifications et redirige vers la page
+        d'accueil.
+    """
     uuid = request.args.get('uuid')
-    todo = liste.get(uuid)[0]
+    try:
+        todo = liste.get(uuid)[0]
+    except:
+        abort(500)
+    
     if request.method == "GET":
         return render_template("modifie.html", todo = todo)
     elif request.method == "POST":
@@ -55,13 +95,18 @@ def modifie():
         todo["etat"] = request.form["etat"]
         liste.modifie(todo)
         return redirect("/")
-    
-    # TODO Exception: géré le cas d'une méthode qui n'est pas la bonne
+    abort(501)
     
     
 @app.route('/filtre', methods=['POST'])
-def filtre():
+def filtre() -> Response:
+    """Applique un filtre défini par l'usager.
+
+    Returns:
+        Response: Retourne la page d'accueil.
+    """
     action = request.form['action']
+    assert(action == 'reset' or action == 'applique')
     if action == 'reset':
         conditions_filtre.reset()
     if action == 'applique':
